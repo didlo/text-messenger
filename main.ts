2ebfc55a5b438453a1e37b7c7d7e1a87419395c5
Douglas <douglas@pittenweem.org.uk>
2025-02-27 18:02:09 +0000

    Added ability to read a string of the sent letters

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber < 26) {
         basic.showString("" + (text_list[receivedNumber]))
+        receivedmessage = "" + receivedmessage + text_list[receivedNumber]
         basic.pause(500)
         basic.clearScreen()
+    } else if (receivedNumber == 75) {
+        basic.showString(receivedmessage)
     } else {
         basic.showIcon(IconNames.Heart)
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+        receivedmessage = ""
         basic.clearScreen()
     }
 })
@@ -16,6 +20,9 @@ input.onButtonPressed(Button.A, function () {
         message = text_list.length - 1
     }
     basic.showString("" + (text_list[message]))
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showString(receivedmessage)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(99)
@@ -28,6 +35,16 @@ input.onButtonPressed(Button.B, function () {
     }
     basic.showString("" + (text_list[message]))
 })
+input.onGesture(Gesture.Shake, function () {
+    receivedmessage = ""
+    for (let index = 0; index < 6; index++) {
+        basic.showIcon(IconNames.No)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(75)
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendNumber(message)
     basic.showIcon(IconNames.Yes)
@@ -35,7 +52,9 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 let text_list: string[] = []
 let message = 0
+let receivedmessage = ""
 radio.setGroup(1)
+receivedmessage = ""
 message = 0
 text_list = [
 "A",
